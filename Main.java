e45c84139a5715f5b7750c0358fa2dc877e71e73
zinashdegefa <mydearbarkot@gmail.com>
2023-04-07 19:08:22 -0500

    Miles and KM

--- Main.java
+++ Main.java
@@ -2,16 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GallonAndLiter gallonObject = new GallonAndLiter("John");
-
-        gallonObject.convertToLiter(1);
-
-        gallonObject.convertToGallon(2);
-
+//        GallonAndLiter gallonObject = new GallonAndLiter("John");
+//
+//        gallonObject.convertToLiter(1);
+//
+//        gallonObject.convertToGallon(2);
+//
         MilesAndKilometers mileObject = new MilesAndKilometers();
-        mileObject.milesToKilometers();
 
-        mileObject.KilometersToMiles();
+        mileObject.milesToKilometers();
+//
+//        mileObject.KilometersToMiles();
 
 
     }
