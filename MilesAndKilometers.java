e1e1486d615cac03786273e5387751572dbdf2c4
zinashdegefa <mydearbarkot@gmail.com>
2023-04-07 21:55:21 -0500

    While loop modified.

--- MilesAndKilometers.java
+++ MilesAndKilometers.java
@@ -1,29 +1,6 @@
 import java.util.Scanner;
 
 public class MilesAndKilometers {
-    Scanner scan = new Scanner(System.in);
-   // Scanner input = new Scanner(System.in);
-   // public  MilesAndKilometers(String name, String mileToKm, int myNu) {
-
-    //}
-
-
-//    public void milesToKilometers() {
-//
-//        System.out.println("Please enter Miles = ");
-//      double mile = scan.nextDouble();
-//      double kilometer = mile *   1.60934;
-//      System.out.println(mile + " Miles is " + kilometer + " Km.");
-//    }
-//
-//    public void KilometersToMiles() {
-//
-//        System.out.println("Please enter Km = ");
-//        double kilometer = scan.nextDouble();
-//        double mile = kilometer * 0.621;
-//        System.out.println(kilometer + " Km is " + mile + " Miles." );
-//
-//    }
 
    public void milesToKilometers() {
 
@@ -34,44 +11,40 @@ public class MilesAndKilometers {
 
       System.out.println("Hello " + name +"! you are here to change Mile to Km or Km to Mile. "   );
 
-       double number = 1;
-//       double Yes = 1;
-//       double No = 2;
-       while (number != 0) {
+         String answer = "";
+       while (!answer.equalsIgnoreCase("No")) {
            System.out.println("What do you want to change ?");
 
            System.out.println("1. mile to km \n2. km to mile");
 
-           String mileToKm = input.next();
+           String check = input.next();
 
-           System.out.println("Enter the number: ");
-           number = scan.nextDouble();
+           if (check.equalsIgnoreCase("1")) {
 
-           if (mileToKm.equalsIgnoreCase("1")) {
-               double kilometer = number * 1.60934;
-               System.out.println(number + " Miles is " + kilometer + " Km.");
+               System.out.println("Enter the number in mile: ");
+               double numberInMile = input.nextDouble();
+               double kilometer = numberInMile * 1.60934;
+               System.out.println(numberInMile + " Miles is " + kilometer + " Km.");
 
-           } else if (mileToKm.equalsIgnoreCase("2")) {
-               double mile = number * 0.621;
-               System.out.println(number + " Km is " + mile + " Miles.");
+           } else if (check.equalsIgnoreCase("2")) {
+
+               System.out.println("Enter the number in km: ");
+               double numberInKm = input.nextDouble();
+               double mile = numberInKm * 0.621;
+               System.out.println(numberInKm + " Km is " + mile + " Miles.");
 
            }  else {
+
                System.out.println("Please enter a valid number (1 or 2)");
+
            }
            System.out.println("Do you want to continue?");
-           String answer = input.next();
+            answer = input.next();
 
-           if (answer.equalsIgnoreCase("NO")) {
-              // scan.close();
-               break;
-           }
        }
-
-
-     System.out.print("Thank you! ");
+       System.out.print("Thank you! ");
 
    }
-
 
 }
 
